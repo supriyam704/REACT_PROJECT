a15a0508ea0d8975b3232d3de2d688774e1e7a76
supriyam704 <supriyamishra704@gmail.com>
2024-08-12 13:24:07 +0530

    setup

---
+++ react-demo/src/components/helloword.jsx
@@ -0,0 +1,13 @@
+// CustomComponent.js
+import React from 'react';
+
+function CustomComponent() {
+  return (
+    <div>
+      <h1>This is a custom component!</h1>
+      <p>This is a paragraph of text.</p>
+    </div>
+  );
+};
+
+export default CustomComponent;
